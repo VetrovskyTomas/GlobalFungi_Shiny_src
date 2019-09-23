1521c8ccee9167834e807861cbb713092182540d
MBU\avetrot <kostelecke.uzeniny@seznam.cz>
2019-09-23 15:18:25 +0200

    default tables path

--- global.R
+++ global.R
@@ -5,8 +5,8 @@ for(p in requiredPackages){
   library(p,character.only = TRUE)
 }
 
-toTrainMetadata <- fread(input = "D:/root/tables/FUNGAL_METASTUDY_Filtered_metadata.txt")
-SNV_counts <- fread(input = "D:/root/tables/Samples_to_SH.txt", header = F)
+toTrainMetadata <- fread(input = "C:/fm_database_root/tables/FUNGAL_METASTUDY_Filtered_metadata.txt")
+SNV_counts <- fread(input = "C:/fm_database_root/tables/Samples_to_SH.txt", header = F)
 
 # Prepare vectors of unique SHs
 # SHs <- unique(SNV_counts$V4)
